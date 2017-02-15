9b06017b947f4089668d77e6f7846cea36de42dc
craig91 <craigdejean1@gmail.com>
2017-02-15 15:34:04 -0500

    Profile page major update

--- models/projects.js
+++ models/projects.js
@@ -1,29 +1,36 @@
-'use strict'; 
+'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Projects = sequelize.define('Projects', {
-    title: { 
-      type: DataTypes.STRING, 
+    title: {
+      type: DataTypes.STRING,
       allowNull: false},
     description: {
       type: DataTypes.TEXT,
       allowNull: false,
       len: [200, 2000]
     },
-    gitUsername: { 
-      type: DataTypes.STRING, 
-      allowNull: false},  
-    gitRepo: { 
-      type: DataTypes.STRING, 
+    gitUsername: {
+      type: DataTypes.STRING,
       allowNull: false},
-    url: { 
-      type: DataTypes.STRING, 
-      allowNull: true, 
-      validate: { 
+    gitRepo: {
+      type: DataTypes.STRING,
+      allowNull: false},
+    url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
         isUrl: true } },
     videoUrl:{
       type: DataTypes.STRING,
       allowNull:true,
       validate:{
+        isUrl:true
+      }
+    },
+    pictureUrl:{
+      type: DataTypes.STRING,
+      allowNull:true,
+      validate: {
         isUrl:true
       }
     },
