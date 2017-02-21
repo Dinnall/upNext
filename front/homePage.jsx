92d09a34a45dab128c598ed6ee60e98afa353826
craig91 <craigdejean1@gmail.com>
2017-02-21 15:22:27 -0500

    deleted temp code

--- front/homePage.jsx
+++ front/homePage.jsx
@@ -28,40 +28,6 @@ const MainPage = React.createClass({
                         </li>
                     </ul>
                 </nav>
-
-
-                {/* <div id="wrapper">
-
-                    <div id="hero">
-
-                        <div className="header">
-                            <h1>
-                                Welcome to my site
-                            </h1>
-                        </div>
-
-                    </div>
-
-                    <div id="content">
-                        <h2>
-                            Creators Thrive
-                        </h2>
-                        <span>
-                            <b>
-                                Here.
-                            </b>
-                        </span>
-                        <p>
-                            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-                        </p>
-                    </div>
-                    <div id="footer">
-                        <p>
-                            I am the footer, and you have scroll down to see me.</p>
-                    </div>
-                </div> */}
-
-                <h1 className='smarter'>A smarter way to learn</h1>
         <div className='secondBorder'></div>
         <div className='thirdBorder'></div>
         <FooterPage />
