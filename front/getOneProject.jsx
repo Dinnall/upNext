b8d66aa549c13f4b3e1a2580fe9a2edb213aa21e
michael <michaeldinnall@gmail.com>
2017-02-24 00:00:54 -0500

    detailed individual page

--- front/getOneProject.jsx
+++ front/getOneProject.jsx
@@ -6,85 +6,131 @@ import {Link} from 'react-router';
 
 const getOneProject = React.createClass({
   getInitialState(){
-    return {project: []}
+    return {projectDetail: []}
   },
   componentDidMount(){
      {
+      var id = this.props.params.id;
       $.ajax({
-        url: '/api/projects',
+        url: '/api/projects'  + id,
         type: "GET"
       })
       .done( (data) => {
-        this.setState({project: data})
+        this.setState({projectDetail: data})
       })
     }
   },
-  render: function() {
-    return (
+  render(){
+    if(this.state.projectDetail){
+    return(
       <div>
-
-        <Link to='/'><img className='page_1' src='../images/page_one.png' /></Link>
-
-
-                            <div className="profile-info">
-
-                            <div className="oval">
-                                <img src={this.state.imageUrl}/>
-                            </div>
-
-                            <div>
-
-                                <h1 className="name">{this.state.firstName} {this.state.lastName}</h1>
-
-                                <p className="location">
-                                    New York, NY
-                                </p>
-
-                                <p className="position">
-                                    Full Stack Web Engineer, C4Q
-                                </p>
-
-                            </div>
-                            <ul className="links">
-                                <li>
-                                    <img src="../images/icn-twitter.png" className="icn-twitter" height="35px" width="35px"></img>
-                                </li>
-
-                                <li>
-                                    <img src="../images/icn-github.png" className="icn-github" height="35px" width="35px"></img>
-                                </li>
-
-                                <li>
-                                    <img src="../images/icn-linkedin.png" className="icn-linkedin" height="35px" width="35px"></img>
-                                </li>
-                                <li>
-                                    <img src="../images/icn-website.png" className="icn-website" height="35px" width="35px"></img>
-                                </li>
-
-                            </ul>
-                            </div>
-
-
-
-
-
-          <div className='secondComp'>
-            {this.state.project.length=== 0 ? "Loading..." : this.state.project.map((projects, idx)=> {
-            return (
-
-              <Link to={"/projects/" + projects.id} key={idx}>
-                <div className="projectGrid">
-                  <p className="projectTitle">{projects.title}</p>
-                  <img  src={projects.pictureUrl} />
-                   </div>
-              </Link>
-                    )
-                     })}
-                </div>
-          </div>
-    )
+        <div>
+           <img src={this.state.projectDetail.pictureUrl} />
+            <p>{this.state.projectDetail.description}</p>
+           <div>
+         </div>
+        </div>
+      </div>
+    )}
+    else{
+      return(
+      <div>
+      <Link to='/'><img className='page_1' src='../images/page_one.png' /></Link>
+      Loading...</div>)
+    }
   }
 })
 
+export default getOneProject;
 
-export default getOneProject
+
+
+
+
+
+// Render with Craigs profile snippet 
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import $ from 'jquery';
+// import {Link} from 'react-router';
+
+
+// const getOneProject = React.createClass({
+//   getInitialState(){
+//     return {projectDetail: []}
+//   },
+//   componentDidMount(){
+//      {
+//       var id = this.props.params.id;
+//       $.ajax({
+//         url: '/api/projects'  + id,
+//         type: "GET"
+//       })
+//       .done( (data) => {
+//         this.setState({projectDetail: data})
+//       })
+//     }
+//   },
+//   render(){
+//     if(this.state.projectDetail){
+//     return(
+//       <div>
+
+//          <div className="profile-info">
+//             <Link to='/'><img className='page_1' src='../images/page_one.png' /></Link>
+
+//                             <div className="oval">
+//                                 <img src={this.state.imageUrl}/>
+//                             </div>
+
+//                             <div>
+
+//                                 <h1 className="name">{this.state.firstName} {this.state.lastName}</h1>
+
+//                                 <p className="location">
+//                                     New York, NY
+//                                 </p>
+
+//                                 <p className="position">
+//                                     Full Stack Web Engineer, C4Q
+//                                 </p>
+
+//                             </div>
+//                             <ul className="links">
+//                                 <li>
+//                                     <img src="../images/icn-twitter.png" className="icn-twitter" height="35px" width="35px"></img>
+//                                 </li>
+
+//                                 <li>
+//                                     <img src="../images/icn-github.png" className="icn-github" height="35px" width="35px"></img>
+//                                 </li>
+
+//                                 <li>
+//                                     <img src="../images/icn-linkedin.png" className="icn-linkedin" height="35px" width="35px"></img>
+//                                 </li>
+//                                 <li>
+//                                     <img src="../images/icn-website.png" className="icn-website" height="35px" width="35px"></img>
+//                                 </li>
+
+//                             </ul>
+//                             </div>
+//         <div>
+//           <img src={this.state.projectDetail.pictureUrl} />
+//           <p>{this.state.projectDetail.description}</p>
+//            <div>
+
+//            </div>
+//         </div>
+//       </div>
+//     )}
+//     else{
+//       return(
+//       <div>
+//     <Link to='/'><img className='page_1' src='../images/page_one.png' /></Link>
+//       Loading...</div>)
+//     }
+//   }
+// })
+
+// export default getOneProject;
--- seed/projects-seed.js
+++ seed/projects-seed.js
@@ -13,9 +13,9 @@ Projects.sync({force: true})
 	  gitRepo: 'Tinder',
 	  url: 'https://github.com/VctrySam/Tinder',
 	  videoUrl: 'https://www.youtube.com/watch?v=FTGa8Tjwfi8',
-		pictureUrl: 'https://gigaom.com/wp-content/uploads/sites/1/2012/12/newlook-1.jpg',
+	  pictureUrl: 'https://gigaom.com/wp-content/uploads/sites/1/2012/12/newlook-1.jpg',
 	  projectId: 1,
-		UserId: 2
+	  UserId: 2
 	},
 	{
 	  title: "Facebook",
@@ -24,9 +24,9 @@ Projects.sync({force: true})
 	  gitRepo: 'soundcloud-redux',
 	  url: 'https://github.com/r-park/soundcloud-redux',
 	  videoUrl: 'https://soundcloud-redux.herokuapp.com/',
-		pictureUrl: 'http://www.businessnewsdaily.com/images/i/000/010/545/i02/Facebook-homepage.PNG?1453478400',
+	  pictureUrl: 'http://www.businessnewsdaily.com/images/i/000/010/545/i02/Facebook-homepage.PNG?1453478400',
 	  projectId: 2,
-		UserId: 1
+	  UserId: 1
 	},
 	{
 	  title: 'Linkedin',
@@ -35,21 +35,21 @@ Projects.sync({force: true})
 	  gitRepo: 'SnapChat',
 	  url: 'https://github.com/VctrySam/Tinder',
 	  videoUrl: 'https://www.youtube.com/watch?v=sStf_ZbTXss',
-		pictureUrl: 'https://www.hotdesign.com/wp-content/uploads/linkedin-personal-profile-600x559.jpg',
+	  pictureUrl: 'https://www.hotdesign.com/wp-content/uploads/linkedin-personal-profile-600x559.jpg',
 	  projectId: 3,
-		UserId: 3
+	  UserId: 3
 	},
 
 	{
 	  title: 'Instagram',
 	  description: 'There are many versions of Javascript, and using React to do Tetris has become my goal.',
-	firstName: 'drake',
+	  firstName: 'drake',
 	  gitRepo: 'react-tetris',
 	  url: 'https://github.com/chvin/react-tetris',
 	  videoUrl: 'https://camo.githubusercontent.com/589fa964de9bab21690a2c5ad036553de296c866/68747470733a2f2f696d672e616c6963646e2e636f6d2f7470732f5442316b764a794f565858585862686146585858585858585858582d3332302d3535352e676966',
-		pictureUrl: 'http://d1y9uiksrn06av.cloudfront.net/wp-content/uploads/2015/02/Embed-Instagram-Screenshot-with-cursor.jpg',
+	  pictureUrl: 'http://d1y9uiksrn06av.cloudfront.net/wp-content/uploads/2015/02/Embed-Instagram-Screenshot-with-cursor.jpg',
 	  projectId: 4,
-		UserId: 1
+	  UserId: 1
 	},
 	{
 	  title: "Twitter",
@@ -58,31 +58,31 @@ Projects.sync({force: true})
 	  gitRepo: '',
 	  url: [''],
 	  videoUrl: 'https://vimeo.com/139013372',
-		pictureUrl: 'http://www.clixmarketing.com/blog/wp-content/uploads/2015/12/Twitter-Ads-1.png',
+	  pictureUrl: 'http://www.clixmarketing.com/blog/wp-content/uploads/2015/12/Twitter-Ads-1.png',
 	  projectId: 5,
-		UserId: 2
+	  UserId: 2
 	},
 	{
 	  title: "Dribble",
 	  description: 'This project looks at loops nested in loops',
-	firstName: 'pikachu',
+	  firstName: 'pikachu',
 	  gitRepo: '',
 	  url: [''],
 	  videoUrl: 'https://vimeo.com/139013372',
-		pictureUrl: 'http://www.evoluted.net/thinktank/wp-content/uploads/2011/05/wb_dribble.jpg',
+	  pictureUrl: 'http://www.evoluted.net/thinktank/wp-content/uploads/2011/05/wb_dribble.jpg',
 	  projectId: 5,
-		UserId: 3
+	  UserId: 3
 	},
 	{
 	  title: "Behance",
 	  description: 'This project looks at loops nested in loops',
-	firstName: 'THOTS',
+	  firstName: 'THOTS',
 	  gitRepo: '',
 	  url: [''],
 	  videoUrl: 'https://vimeo.com/139013372',
-		pictureUrl: 'http://www.carstockphotography.co.uk/news/behance%20jan2013/behance%20page%20tim%20wallace.jpg',
+	  pictureUrl: 'http://www.carstockphotography.co.uk/news/behance%20jan2013/behance%20page%20tim%20wallace.jpg',
 	  projectId: 5,
-		UserId: 1
+	  UserId: 1
 	},
 	{
 	  title: "Myspace",
@@ -91,9 +91,9 @@ Projects.sync({force: true})
 	  gitRepo: '',
 	  url: [''],
 	  videoUrl: 'https://vimeo.com/139013372',
-		pictureUrl: 'http://www.hugpug.com/photos/sheba_layout5/myspace.jpg',
+	  pictureUrl: 'http://www.hugpug.com/photos/sheba_layout5/myspace.jpg',
 	  projectId: 5,
-		UserId: 2
+	  UserId: 2
 	},
 	{
 	  title: "Quora",
@@ -102,9 +102,9 @@ Projects.sync({force: true})
 	  gitRepo: '',
 	  url: [''],
 	  videoUrl: 'https://vimeo.com/139013372',
-		pictureUrl: 'https://blog.kissmetrics.com/wp-content/uploads/2012/07/quora-related-questions.png',
+	  pictureUrl: 'https://blog.kissmetrics.com/wp-content/uploads/2012/07/quora-related-questions.png',
 	  projectId: 5,
-		UserId: 3
+	  UserId: 3
 	},
 ]))
 
